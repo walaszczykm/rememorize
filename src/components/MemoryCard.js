a5ca96545276c57f6ebf2e6e36f1eeba282af1d9
Maciej Walaszczyk <walaszczykm@gmail.com>
2017-12-23 18:40:16 +0100

    handling upload of multiple files

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import firebase from 'firebase'
 
-export default ({ memory }) => {
-  return (
-    <Card>
-      <Image size='medium' src='https://www.visitoslo.com//Images/Bilder%20Oslo/Attraksjoner/Akrobaten_Rolf%20Thoresen.jpg?t=ScaleDownToFill|1500x1500' />
-      <Card.Content>
-        <Card.Header>
-          <Link to={`/memory/${memory.id}`}>{memory.name}</Link>
-        </Card.Header>
-        <Card.Meta>{memory.date && memory.date.toString()}</Card.Meta>
-        <Card.Description>{memory.description}</Card.Description>
-      </Card.Content>
-    </Card>
-  )
+export default class MemoryCard extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      photoUrl: `${process.env.PUBLIC_URL}/missing-image.png`
+    }
+  }
+
+  componentDidMount () {
+    const { photo } = this.props.memory
+    photo && firebase.storage().ref()
+    .child(`media/${photo}`)
+    .getDownloadURL()
+    .then(url => this.setState({ photoUrl: url }))
+  }
+
+  render () {
+    const { memory } = this.props
+    const { photoUrl } = this.state
+
+    return (
+      <Card>
+        <Image size='medium' src={photoUrl} />
+        <Card.Content>
+          <Card.Header>
+            <Link to={`/memory/${memory.id}`}>{memory.name}</Link>
+          </Card.Header>
+          <Card.Meta>{memory.date && memory.date.toString()}</Card.Meta>
+          <Card.Description>{memory.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  }
 }
--- src/components/MemoryForm.js
+++ src/components/MemoryForm.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import { Form } from 'semantic-ui-react'
+import { Form, Image, Grid } from 'semantic-ui-react'
+import FileInput from './FileInput'
+import firebase from 'firebase'
 
 export default class MemoryForm extends Component {
   constructor (props) {
     super(props)
-    const { name, description, date } = props.memory || {}
+    const { name, description, date, photo, media, photoUrl } = props.memory || {}
     this.state = {
       name: name || '',
       description: description || '',
-      date: date || ''
+      date: date || '',
+      photo: photo || '',
+      media: media || [],
+      photoUrl: photoUrl || `${process.env.PUBLIC_URL}/missing-image.png`
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.onNewMedia = this.onNewMedia.bind(this)
   }
 
   handleChange (e, { name, value }) {
@@ -21,20 +27,49 @@ export default class MemoryForm extends Component {
 
   handleSubmit () {
     this.setState({ loading: true })
-    this.props.onSubmit({ ...this.state })
+    const { name, description, date, photo } = this.state
+    this.props.onSubmit({
+      name,
+      description,
+      date,
+      photo
+    })
+  }
+
+  onNewMedia (id, url) {
+    const { media } = this.state
+    this.setState({ media: [...media, id], photo: id, photoUrl: url })
+  }
+
+  componentDidMount () {
+    const { photo } = this.state
+    photo && firebase.storage().ref()
+    .child(`media/${photo}`)
+    .getDownloadURL()
+    .then(url => this.setState({ photoUrl: url }))
   }
 
   render () {
-    const { name, description, date } = this.state
+    const { name, description, date, photoUrl } = this.state
     const { loading, submitText } = this.props
 
     return (
-      <Form loading={loading} onSubmit={this.handleSubmit}>
-        <Form.Input placeholder='Nazwa wspomnienia...' name='name' value={name} onChange={this.handleChange} />
-        <Form.TextArea placeholder='Opis wspomnienia...' name='description' value={description} onChange={this.handleChange} />
-        <Form.Input placeholder='Data...' name='date' value={date} onChange={this.handleChange} />
-        <Form.Button>{submitText}</Form.Button>
-      </Form>
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <Image fluid rounded src={photoUrl} />
+          </Grid.Column>
+          <Grid.Column>
+            <Form loading={loading} onSubmit={this.handleSubmit}>
+              <Form.Input placeholder='Nazwa wspomnienia...' name='name' value={name} onChange={this.handleChange} />
+              <Form.TextArea placeholder='Opis wspomnienia...' name='description' value={description} onChange={this.handleChange} />
+              <Form.Input placeholder='Data...' name='date' value={date} onChange={this.handleChange} />
+              <FileInput buttonContent='Wybierz pliki' filesPrefix='media' onFileUploaded={this.onNewMedia} />
+              <Form.Button>{submitText}</Form.Button>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
